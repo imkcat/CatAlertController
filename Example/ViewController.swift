158a904b6411db5cb1bf2ada1db04036edacc314
ImKcat <kcatdeveloper@icloud.com>
2018-06-27 09:56:06 +0800

    More example

--- Example/ViewController.swift
+++ Example/ViewController.swift
@@ -19,20 +19,41 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
     }
     
-    @IBAction func alertStyleExampleAction(_ sender: Any) {
-        AlertController(title: "Alert Style", message: "This is AlertController", preferredStyle: UIAlertControllerStyle.alert)
+    @IBAction func alertPopupExampleAction(_ sender: Any) {
+        AlertController(title: "Popup", message: "Alert Popup Example", preferredStyle: UIAlertControllerStyle.alert)
+            .flash(from: self, animated: true, delay: 1, completion: nil)
+    }
+    
+    @IBAction func alertStyleFlashExampleAction(_ sender: Any) {
+        AlertController(title: "Alert", message: "Alert Style Flash Example", preferredStyle: UIAlertControllerStyle.alert)
             .addAction(UIAlertAction(title: "Cancel", style: UIAlertActionStyle.cancel, handler: nil))
             .addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: nil))
             .flash(from: self, animated: true, delay: 1, completion: nil)
     }
     
-    @IBAction func actionSheetStyleExampleAction(_ sender: Any) {
-        AlertController(title: "Action Sheet Style", message: "This is AlertController", preferredStyle: UIAlertControllerStyle.actionSheet)
+    @IBAction func alertStyleExampleAction(_ sender: Any) {
+        AlertController(title: "Alert", message: "Alert Style Example", preferredStyle: UIAlertControllerStyle.alert)
+            .addAction(UIAlertAction(title: "Cancel", style: UIAlertActionStyle.cancel, handler: nil))
+            .addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: nil))
+            .present(from: self, animated: true)
+    }
+    
+    @IBAction func actionSheetStyleFlashExampleAction(_ sender: Any) {
+        AlertController(title: "Action Sheet", message: "Action Sheet Style Flash Example", preferredStyle: UIAlertControllerStyle.actionSheet)
             .addAction(UIAlertAction(title: "Item 1", style: UIAlertActionStyle.default, handler: nil))
             .addAction(UIAlertAction(title: "Item 2", style: UIAlertActionStyle.default, handler: nil))
             .addAction(UIAlertAction(title: "Item 3", style: UIAlertActionStyle.default, handler: nil))
             .addAction(UIAlertAction(title: "Cancel", style: UIAlertActionStyle.cancel, handler: nil))
             .flash(from: self, animated: true, delay: 1, completion: nil)
+    }
+    
+    @IBAction func actionSheetStyleExampleAction(_ sender: Any) {
+        AlertController(title: "Action Sheet", message: "Action Sheet Style Example", preferredStyle: UIAlertControllerStyle.actionSheet)
+            .addAction(UIAlertAction(title: "Item 1", style: UIAlertActionStyle.default, handler: nil))
+            .addAction(UIAlertAction(title: "Item 2", style: UIAlertActionStyle.default, handler: nil))
+            .addAction(UIAlertAction(title: "Item 3", style: UIAlertActionStyle.default, handler: nil))
+            .addAction(UIAlertAction(title: "Cancel", style: UIAlertActionStyle.cancel, handler: nil))
+            .present(from: self, animated: true)
     }
     
 }
